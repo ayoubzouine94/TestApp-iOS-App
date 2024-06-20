481e897c055716a03b1fef74d8f941d47ae31a41
ayoubzouine94 <ayoubzouine94@gmail.com>
2024-06-20 23:02:15 +0100

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Ayoub on 20/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
